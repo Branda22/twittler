9accb824ddff92a29b55572091d849d82086ef9a
Christian Brandalise <christian.brandalise@gmail.com>
2015-02-17 14:07:19 -0200

    displaying users messages

--- application.js
+++ application.js
@@ -1,35 +1,45 @@
 $(document).ready(function(){
   var $section = $('body').find('section');
   $section.html('');
-
   function buildTweet(index){
       var tweet = streams.home[index];
+      var userMessages = streams.users[tweet.user];
+      var idx = userMessages.length - 1;
       var $tweet = $('<div></div>').addClass('tweet');
       var $user = $('<a href="#">@' + tweet.user +'</a>').addClass('user');
       var $message = $('<p>' + tweet.message + '</p>').addClass('message');
       var $time = $('<p>'+ tweet.created_at +'</p>').addClass('time');
+
+      var $timeline = $('<div></div>').addClass('timeline'); 
+      for(idx; idx >= 0; idx--){
+        var msg = userMessages[idx]["message"];
+        $timeline.append($('<p>' + msg + '</p>'));
+      }
       $tweet.append($user);
       $tweet.append($message);
       $tweet.append($time);
-
+      $tweet.append($timeline);
       $tweet.appendTo($section);
 
   }
   
-  function getTweet(){
+  function getTweets(){
     var index = streams.home.length - 1;
-    buildTweet(index);
+    for(index; index >= 0; index--){
+        buildTweet(index);         
+    }
   }
   //get initial tweets to load page;
-  getTweet();
+  getTweets();
 
   //auto load tweets every random amount of time.
   setInterval(function(){
-    getTweet();
-  }, 3000);
+    getTweets();
+  }, 5000);
 
-  $('.user').on('click', function(){
-        
+  $('.tweet').on('click','.user', function(){
+    $(this).find(".message").append('<p>hello</p>').slideToggle();    
   });
+  
   $("html").load()    
 });
