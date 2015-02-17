9238b538938372491e525f407e747fad92ef1c00
Christian Brandalise <christian.brandalise@gmail.com>
2015-02-17 00:40:12 -0200

    added setInterval

--- application.js
+++ application.js
@@ -1,20 +1,25 @@
-      function loadTweets(tweet){
-        setInterval(function(){
+$(document).ready(function(){
+  var $body = $('body');
+  $body.html('');
 
-        }, 1000);
-      }
+  function getTweets(){
+    var index = streams.home.length - 1;
+    while(index >= 0){
+      var tweet = streams.home[index];
+      var $tweet = $('<div></div>');
+      var $userID = $('<span>@' + tweet.user + '</span>');
+      $tweet.text( $userID + ": " + tweet.message + ' ' + ' | created at ' + tweet.created_at);
+      $tweet.appendTo($body);
+      index -= 1;
+    }
+  }
+  //get initial tweets to load page;
+  getTweets();
 
-      $(document).ready(function(){
-        var $body = $('body');
-        $body.html('');
+  //auto load tweets every 3000ms
+  setInterval(function(){
+    getTweets();
+  }, 3000);
 
-        var index = streams.home.length - 1;
-        while(index >= 0){
-          var tweet = streams.home[index];
-          var $tweet = $('<div></div>');
-          $tweet.text('@' + tweet.user + ': ' + tweet.message + ' ' + tweet.created_at);
-          $tweet.appendTo($body);
-          index -= 1;
-        }
-
-      });
+  $("html").load()    
+});
