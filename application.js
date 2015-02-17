ad84de2bc80062276b29aed0c6055ea643d0c65a
Christian Brandalise <christian.brandalise@gmail.com>
2015-02-17 16:37:07 -0200

    show users tweets

--- application.js
+++ application.js
@@ -6,15 +6,18 @@ $(document).ready(function(){
       var userMessages = streams.users[tweet.user];
       var idx = userMessages.length - 1;
       var $tweet = $('<div></div>').addClass('tweet');
-      var $user = $('<a href="#">@' + tweet.user +'</a>').addClass('user');
+      var $user = $('<a>@' + tweet.user +'</a>').addClass('user');
       var $message = $('<p>' + tweet.message + '</p>').addClass('message');
       var $time = $('<p>'+ tweet.created_at +'</p>').addClass('time');
 
-      var $timeline = $('<div></div>').addClass('timeline'); 
+      var $timeline = $('<div>Your Twitls</div>').addClass('timeline');
       for(idx; idx >= 0; idx--){
-        var msg = userMessages[idx]["message"];
-        $timeline.append($('<p>' + msg + '</p>'));
+        var $msg = $('<p>' + userMessages[idx]["message"] + '</p>').addClass("timeline-message");
+        var $tme = $('<p>' + userMessages[idx]["created_at"] + '</p>').addClass("timeline-time"); 
+        $timeline.append($msg);
+        $timeline.append($tme);
       }
+
       $tweet.append($user);
       $tweet.append($message);
       $tweet.append($time);
@@ -29,16 +32,22 @@ $(document).ready(function(){
         buildTweet(index);         
     }
   }
-  //get initial tweets to load page;
+
+  //Render the inital tweets on the page.
   getTweets();
 
-  //auto load tweets every random amount of time.
-  setInterval(function(){
-    getTweets();
-  }, 5000);
+  //Load tweets when the user scrolls the browser.
+  //The user will never get to the end because data_generator.js
+  //Never stops loading tweets.
+  $(window).on('scroll', function(event){
+    event.preventDefault();
+    if($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
+       getTweets();
+    }  
+  });
 
-  $('.tweet').on('click','.user', function(){
-    $(this).find(".message").append('<p>hello</p>').slideToggle();    
+  $('.tweet').on('click', function(event){
+    $(this).find(".timeline").slideToggle();
   });
   
   $("html").load()    
